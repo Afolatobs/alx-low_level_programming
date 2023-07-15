c7dce40fd46cdd83714472c001542c1ee97498bc
Afolatobs <thowhseen10gmail.com>
2023-07-14 17:49:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Forward 0(success)
+ *
+ */
+
+int main(void)
+{
+char p;
+p = 'b';
+
+while
+	(p <= 'y'){
+putchar (p);
+p++;
+}
+
+putchar('\n');
+return(0);
+}    
