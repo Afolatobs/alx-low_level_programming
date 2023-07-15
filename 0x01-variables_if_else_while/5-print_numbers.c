ed601c0761730fc023650fa774ac3317f2a4f8c1
Afolatobs <thowhseen10gmail.com>
2023-07-14 18:46:53 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main Entry
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+putchar(digit);
+
+putchar('\n');
+return (0);
+}
