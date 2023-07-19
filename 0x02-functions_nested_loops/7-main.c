e5743a66bb5ca6a7884c82181a4f80f422b0a007
Afolatobs <thowhseen10gmail.com>
2023-07-18 19:00:53 -0700

    last_digit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r =(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
