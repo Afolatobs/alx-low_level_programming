c2ce55c6772e3f9f5fa9848caa8f7aafbda40833
Afolatobs <thowhseen10gmail.com>
2023-07-24 18:14:14 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
