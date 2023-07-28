0318e4a94242b9bd349d8f968c8e53eabd3a49f4
Afolatobs <thowhseen10gmail.com>
2023-07-28 14:30:48 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,25 +2,22 @@
 
 /**
  * main - main Entry
- *
  * Return: Always 0 (success)
  */
-
 
 int main(void)
 {
 int h;
 
-for (h = 0; h < 10; h++)
-
+for (h = 0 ; h < 10 ; h++)
 {
-putchar(h + 10);
+putchar(h + '0');
 if (h < 9)
-
+{
 putchar(',');
-putchar(',');
+putchar(' ');
 }
-
+}
 putchar('\n');
 return (0);
 }
