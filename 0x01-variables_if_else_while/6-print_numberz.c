2d2df0485493a9d580b7386a0b91f2a0b9e61d30
Afolatobs <thowhseen10gmail.com>
2023-07-28 14:10:11 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,22 +2,19 @@
 
 /**
  * main - main Entry
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 int h;
-
 h = 0;
 
-while
-(h < 10)	{
-putchar (h + 0);
+while (h < 10)
+{
+putchar (h + '0');
 h++;
 }
-
 putchar('\n');
 return (0);
 }
