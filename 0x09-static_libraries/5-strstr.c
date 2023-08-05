38e93611deeab29169f2159bf346c6c332675dd4
Afolatobs <thowhseen10gmail.com>
2023-08-05 07:10:15 -0700

    0x09-st-static_libraries-01

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - program entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0' ; haystack++)
+	{
+		char *f = haystack;
+		char *g = needle;
+
+		while (*f == *g && *g != '\0')
+		{
+			f++;
+			g++;
+		}
+		if (*g == '\0')
+			return (haystack);
+	}
+	return (0);
+}
