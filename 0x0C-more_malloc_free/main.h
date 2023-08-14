24bc5697ca23d61c00b27054abdea17552273c46
Afolatobs <thowhseen10gmail.com>
2023-08-14 08:50:43 -0700

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,10 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *t, char p, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int main(int argc, char *argv[]);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+int _putchar(char c);
 
 #endif
