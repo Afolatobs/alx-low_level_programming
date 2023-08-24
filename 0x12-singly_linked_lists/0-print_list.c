d4b140009d4e23ed85933770adc6c5f72ae965ed
Afolatobs <thowhseen10gmail.com>
2023-08-24 16:14:35 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,39 +2,22 @@
 
 /**
  * print_list - function
- * main - function
- * @h: list_t
- * @list_t: node
- * Return: 0
+ * @h: node
+ * Return: list_t
  */
 
 size_t print_list(const list_t *h)
 {
-	if (str == NULL)
+	while (h)
 	{
+	if (h->str == NULL)
 	printf("[0] (nil)\n");
-	}
 
 	else
-	{
-		printf("the list_t is empty.\n");
+	printf("the list_t is empty.\n");
+	h++;
 	}
-
-	return (list_t);
-}
-
-int main(void)
-{
-	list_t *h;
-	list_t *dou;
-	list_t *trois;
-
-	h->len = NULL;
-	h->next = dou;
-	dou->len == NULL;
-	dou->next == trois;
-	trois->len == NULL;
-	trois->next == NULL;
 
 	return (0);
 }
+
