cef929bba1de86e93c76ae409f7c272af93d3f92
Afolatobs <thowhseen10gmail.com>
2023-08-24 16:59:48 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,17 @@
 size_t print_list(const list_t *h)
 {
 	size_t trois = 0;
-	
+
 	while (h)
 	{
 	if (!h->str)
 	printf("[0] (nil)\n");
 	else
 	printf("[%u] %s\n", h->len, h->str);
+	}
 
 	trois++;
-	}
+
 
 	return (0);
 }
