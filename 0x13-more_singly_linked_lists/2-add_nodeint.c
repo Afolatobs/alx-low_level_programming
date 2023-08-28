78c246634753a9a16127b009e95b956a54bce7fe
Afolatobs <thowhseen10@gmail.com>
2023-08-28 15:30:58 +0000

    update

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - the main funtion
+ * @n: the element data
+ * @head: the first head
+ *
+ * Return: the new element address
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *adnote = malloc(sizeof(listint_t));
+
+	if (adnote != NULL)
+	{
+	adnote->n = n;
+	adnote->next = *head;
+	*head = adnote;
+	}
+
+	return (adnote);
+}
