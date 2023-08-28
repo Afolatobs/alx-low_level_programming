8c9879a397640f28da7abea27d49594989fd5330
Afolatobs <thowhseen10@gmail.com>
2023-08-28 14:15:44 +0000

    update

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - the main function
+ * @h: the head node
+ *
+ * Return: number of node
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+	
+	while (h)
+	{
+	h = h->next;
+	n++;
+	}
+
+	return (n);
+}
