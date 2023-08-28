079ec6570c1807aa1c6f7fd67e556bd9f3b8e1be
Afolatobs <thowhseen10@gmail.com>
2023-08-28 10:33:58 +0000

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - the main function
+ * @h: head node
+ *
+ * Return: 0
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+	if (h->next == NULL)
+	n++;
+
+	printf("%d\n", h->n);
+	h = h->next;
+
+	}
+
+	return n;
+}
