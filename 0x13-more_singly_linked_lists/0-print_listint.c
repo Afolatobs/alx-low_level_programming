eaea111f8b6df4b87da6ccf20b545b505c12e14b
Afolatobs <thowhseen10@gmail.com>
2023-08-29 06:54:13 +0000

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,13 +13,12 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-	if (h->next == NULL || h->next != NULL)
-	{
-	printf("%d\n", h->n);
-	h = h->next;
-	n++;
+		if (h->next == NULL || h->next != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			n++;
+		}
 	}
-	}
-
 	return (n);
 }
