3c0b39e90b77742cdee85737dcb4c390e281e156
Afolatobs <thowhseen10@gmail.com>
2023-08-29 08:44:38 +0000

    update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,9 +13,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *adnote = malloc(sizeof(listint_t));
 
 	if (adnote != NULL)
+	{
 	adnote->n = n;
 	adnote->next = *head;
 	*head = adnote;
-
+	}
 	return (adnote);
 }
