dfb8640db8ce77136c0a4faaaaec15e7503dfbe4
Afolatobs <thowhseen10@gmail.com>
2023-08-29 10:34:21 +0000

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,6 +4,7 @@
  * add_nodeint_end - the main function
  * @head: the old element
  * @n: the data in the element
+ * @cate: temp file
  *
  * Return: the new element
  */
@@ -12,14 +13,23 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *nat_ol = malloc(sizeof(listint_t));
 
-	if (nat_ol != NULL)
+	if (!nat_ol)
+	{
+		return (NULL);
+	}
 
 	nat_ol->n = n;
 	nat_ol->next = NULL;
 
-	if (*head != NULL)
-	nat_ol->next = *head;
-	*head = nat_ol;
+	if (*head == NULL)
+		*head = nat_ol;
+	else
+	{
+	listint_t *cate = *head;
+	while (cate->next != NULL)
+		cate = cate->next;
 
+	cate->next = nat_ol;
+	}
 	return (nat_ol);
 }
